253b413a435d1718af614f7e32ec8c4629acbb33
user <asadbektemirov97>
2022-04-04 09:08:38 +0500

    Initial commit

---
+++ src/main/java/com/example/instagramfullrestapi/InstagramFullRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagramfullrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramFullRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramFullRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/instagramfullrestapi/InstagramFullRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instagramfullrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramFullRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
